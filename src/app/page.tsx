4f41299106038ebdc39a22ffce8b7c1f9f03c08d
camziny <cameronziny@gmail.com>
2024-06-03 17:19:10 -0400

    - render product data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { db } from "./server/db/schema";
 
+export const dynamic = "force-dynamic";
+
 const imageUrls = [
   "https://utfs.io/f/69cffd4e-1b68-4ae1-9d94-c9958da17ac9-fsrxvh.jpeg",
 ];
@@ -11,7 +13,9 @@ const images = imageUrls.map((url, index) => ({
 }));
 
 export default async function Home() {
-  const products = await db.query.product.findMany();
+  const products = await db.query.product.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
@@ -26,8 +30,17 @@ export default async function Home() {
         ))}
       </div>
       <div>
+        <h1 className="text-3xl font-bold">Products</h1>
         {products.map((product) => (
-          <div key={product.id}> {product.title} </div>
+          <div key={product.id} className="flex w-100 flex-col p-4">
+            <div className="text-xl font-semibold">{product.title}</div>{" "}
+            <img
+              src={product.image}
+              alt="image"
+              className="object-contain w-48 h-96"
+            ></img>{" "}
+            {product.description}
+          </div>
         ))}
       </div>
     </main>
