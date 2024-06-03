9f0b7e57813adcb5d255573ab0f78bf6edd0be8a
camziny <cameronziny@gmail.com>
2024-06-03 17:07:32 -0400

    - fetch data from db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/schema.ts",
+  schema: "./src/app/server/db/schema.ts",
   out: "./drizzle",
+  dbCredentials: {
+    url: process.env.POSTGRES_URL as string,
+  },
 });
