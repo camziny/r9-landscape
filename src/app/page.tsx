4536f31abd4151ad27f4d63548e9adbd04097af0
camziny <cameronziny@gmail.com>
2024-06-03 16:31:01 -0400

    - add nav bar function - image on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Image from "next/image";
 
+const imageUrls = [
+  "https://utfs.io/f/69cffd4e-1b68-4ae1-9d94-c9958da17ac9-fsrxvh.jpeg",
+];
+
+const images = imageUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Route 9 landscape
+    <main className="">
+      <h1 className="text-3xl font-bold text-center text-gray-800">
+        Route 9 Landscape Products
+      </h1>
+      <div className="flex flex-wrap justify-center">
+        {images.map((image) => (
+          <div key={image.id} className="w-1/2 p-4">
+            <img src={image.url} alt="image" className="object-fill" />
+          </div>
+        ))}
       </div>
     </main>
   );
